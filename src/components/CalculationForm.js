40566e838617047370e64e67efd72ce736d8b839
preservedfish <71352883+preservedfish@users.noreply.github.com>
2021-02-27 15:35:43 -0500

    Refactor calculator form and list into components

---
+++ src/components/CalculationForm.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Form, InputGroup, Button } from 'react-bootstrap';
+
+const CalculationForm = ({
+  addCalculation,
+  newCalculation,
+  setNewCalculation,
+}) => (
+  <Form className="w-50 mb-3" onSubmit={addCalculation}>
+    <InputGroup>
+      <Form.Control
+        type="text"
+        placeholder="E.g. 2+2"
+        value={newCalculation}
+        onChange={(event) => setNewCalculation(event.target.value)}
+      />
+      <InputGroup.Append>
+        <Button type="submit" variant="outline-primary">
+          Calculate
+        </Button>
+      </InputGroup.Append>
+    </InputGroup>
+    <Form.Text>
+      You can use operators such as +, -, *, **, /, %. Parentheses and decimals
+      work too.
+    </Form.Text>
+  </Form>
+);
+
+CalculationForm.propTypes = {
+  addCalculation: PropTypes.func.isRequired,
+  newCalculation: PropTypes.string.isRequired,
+  setNewCalculation: PropTypes.func.isRequired,
+};
+
+export default CalculationForm;
